ace0b7c4cd3038890810d09357bf671c2abcd88d
David Koenig <koenigdavidmj@gmail.com>
2016-05-27 21:00:15 -0700

    Add KJV, manifest file, and book stats methods.

--- app.js
+++ app.js
@@ -1,11 +1,34 @@
+var fs = require('fs');
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
+var manifest = [];
+var books = JSON.parse(fs.readFileSync('kjv.manifest'));
+for (var i = 0; i < books.length; ++i) {
+    manifest.push(books[i]);
+}
+
+app.get('/', function(req, res) {
       res.send('Hello World!');
 });
 
-app.listen(3000, function () {
-      console.log('Example app listening on port 3000!');
+app.get('/books', function(req, res) {
+    var rv = [];
+    for (var i = 0; i < manifest.length; ++i) {
+        rv.push(manifest[i].name);
+    }
+    res.json({ "items": rv });
 });
+
+app.get('/book/:name', function(req, res, next) {
+    for (var i = 0; i < manifest.length; ++i) {
+        if (manifest[i].name == req.params.name) {
+            res.json({ "chapters": manifest[i].chapters });
+            return;
+        }
+    }
+    next();
+});
+
+app.listen(3000);
 
